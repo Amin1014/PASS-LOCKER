f357094998524bcab5274b4849b1cf5b898e2f5f
Amin1014 <aminabdi570@gmail.com>
2021-07-31 06:56:07 +0300

    test case to initialize properly

--- credentials_test.py
+++ credentials_test.py
@@ -6,3 +6,13 @@ class TestAccount(unittest.TestCase):
     def setUp(self):
        
         self.new_account = Account("Amin","Moha","123456","moha.m@.com") # create Account object
+
+    def test_init(self):
+        '''
+        test_init test case to test if the object is initialized properly
+        '''
+
+        self.assertEqual(self.new_account.account_name,"Amin")
+        self.assertEqual(self.new_account.user_name,"Moha")
+        self.assertEqual(self.new_account.password,"123456")
+        self.assertEqual(self.new_account.email,"moha.m@.com")
