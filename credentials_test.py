19faad3432b770d6f631a8617d8991efdf23a5cc
Amin1014 <aminabdi570@gmail.com>
2021-07-31 07:01:23 +0300

    test saving new account

--- credentials_test.py
+++ credentials_test.py
@@ -16,3 +16,11 @@ class TestAccount(unittest.TestCase):
         self.assertEqual(self.new_account.user_name,"Moha")
         self.assertEqual(self.new_account.password,"123456")
         self.assertEqual(self.new_account.email,"moha.m@.com")
+
+    def test_save_account(self):
+        '''
+        test_save_account test case to test if the account object is saved into
+         the account list
+        '''
+        self.new_account.save_account() # saving the new account
+        self.assertEqual(len(Account.account_list),1)  
